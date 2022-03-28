c972890215f97c544e8b9bbd8b1d04077a93302a
Uhurk <martinvrablec18@gmail.com>
2022-03-27 21:28:10 -0700

    a
    a

--- bazarik/app/Providers/AppServiceProvider.php
+++ bazarik/app/Providers/AppServiceProvider.php
@@ -3,9 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,7 +15,6 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-        Schema::defaultStringLength(191);
     }
 
     /**
--- bazarik/config/database.php
+++ bazarik/config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => 'mysql',
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -45,13 +45,13 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => 'DATABASE_URL',
-            'host' =>  'local',
-            'port' => '3306',
-            'database' => 'bazar',
-            'username' => 'bazar',
-            'password' => 'root',
-            'unix_socket' => '',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
