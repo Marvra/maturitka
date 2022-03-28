03d4d6dee3f3f4844855fb18edfc03adbd7f0619
Uhurk <martinvrablec18@gmail.com>
2022-03-27 20:01:38 -0700

    heroku database
    zmena v database.php pre heroku

--- bazarik/config/database.php
+++ bazarik/config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'mysql',
 
     /*
     |--------------------------------------------------------------------------
@@ -45,13 +45,13 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'url' => 'DATABASE_URL',
+            'host' =>  '127.0.0.1',
+            'port' => '3306',
+            'database' => 'forge',
+            'username' => 'root',
+            'password' => 'root',
+            'unix_socket' => '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
