c77487495dade60ec226af2442ddfa7456303aad
Uhurk <martinvrablec18@gmail.com>
2022-03-27 23:17:18 -0700

    g
    g

--- bazarik/database/migrations/2022_02_15_053509_create_posts_table.php
+++ bazarik/database/migrations/2022_02_15_053509_create_posts_table.php
@@ -23,6 +23,7 @@ class CreatePostsTable extends Migration
             $table->string('contact')->unique();
             $table->string('location');
             $table->timestamp('created_at');
+            $table->timestamp('updated_at');
             $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')->references('id')->on('categories');
 
